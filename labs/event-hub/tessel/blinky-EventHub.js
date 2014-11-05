68d8c36bd1975c2cf587d0ba1a2e3b8eb1acaaa1
Robert Eichenseer <roberteichenseer@live.de>
2014-11-05 13:47:26 +0100

    Minor code change

--- labs/event-hub/tessel/blinky-EventHub.js
+++ labs/event-hub/tessel/blinky-EventHub.js
@@ -1,62 +1,49 @@
 var https = require('https');
 var crypto = require('crypto');
-var moment = require('moment');
 
 // Event Hubs parameters
+console.log('----------------------------------------------------------------------');
+console.log('Please ensure that you have modified the values for: namespace, hubname, partitionKey, eventHubAccessKeyName');
+console.log('Please ensure that you have created a Shared Access Signature Token and you are using it in the code')
+console.log('----------------------------------------------------------------------');
+console.log(''); 
+
 var namespace = 'RobEichDevEventHub-ns';
 var hubname ='demoeventhub';
-var devicename = 'mytessel';
+var partitionKey = 'mytessel';
+var eventHubAccessKeyName = 'EventHubKey';
+var createdSAS = 'SharedAccessSignature sr=https%3A%2F%2FRobEichDevEventHub-ns.servicebus.windows.net%2Fdemoeventhub%2Fpublishers%2Fmytessel%2Fmessages&sig=cmwmWeUD%2FBvXC%2FnzgPqbe1Kx4TB8NrksSZrOQK%2FjbaA%3D&se=1417774602&skn=EventHubKey';
+
+console.log('Namespace: ' + namespace);
+console.log('hubname: ' + hubname);
+console.log('partitionKey: ' + partitionKey);
+console.log('eventHubAccessKeyName: ' + eventHubAccessKeyName);
+console.log('SAS Token: ' + createdSAS);
+console.log('----------------------------------------------------------------------');
+console.log('');
  
 // Payload to send
 var payload = '{\"Temperature\":\"37.0\",\"Humidity\":\"0.4\"}';
- 
-// Shared access key (from Event Hub configuration)
-var my_key_name = 'EventHubKey';
-var my_key = 'mlHhCj6aAVgILAtwrxqnBSL5pfa5yJD3nhbN8CrTWZ0=';
- 
-// Full Event Hub publisher URI
-var my_uri = 'https://' + namespace + '.servicebus.windows.net' + '/' + hubname + '/publishers/' + devicename + '/messages';
- 
- 
-// function create_sas_token(uri, key_name, key)
-// {
-//     // Token expires in one hour
-//     var expiry = moment().add(1, 'months').unix();
-//     //var expiry = '1415185462';
-//     console.log(expiry);
- 
-//     var string_to_sign = encodeURIComponent(uri) + '\n' + expiry;
-//     var hmac = crypto.createHmac('sha256', key);
-//     hmac.update(string_to_sign);
-//     var signature = hmac.digest('base64');
-//     var token = 'SharedAccessSignature sr=' + encodeURIComponent(uri) + '&sig=' + encodeURIComponent(signature) + '&se=' + expiry + '&skn=' + key_name;
- 
-//     return token;
-// }
- 
-//var my_sas = create_sas_token(my_uri, my_key_name, my_key)
-var my_sas = 'SharedAccessSignature sr=https%3A%2F%2FRobEichDevEventHub-ns.servicebus.windows.net%2Fdemoeventhub%2Fpublishers%2Fmytessel%2Fmessages&sig=cmwmWeUD%2FBvXC%2FnzgPqbe1Kx4TB8NrksSZrOQK%2FjbaA%3D&se=1417774602&skn=EventHubKey';
+
  
 // Send the request to the Event Hub
- 
 var options = {
   hostname: namespace + '.servicebus.windows.net',
   port: 443,
-  path: '/' + hubname + '/publishers/' + devicename + '/messages',
+  path: '/' + hubname + '/publishers/' + partitionKey + '/messages',
   method: 'POST',
   headers: {
-    'Authorization': my_sas,
+    'Authorization': createdSAS,
     'Content-Length': payload.length,
     'Content-Type': 'application/atom+xml;type=entry;charset=utf-8'
   }
 };
 
-//console.log(options);
- 
 var req = https.request(options, function(res) {
+  console.log('----------------------------------------------------------------------');
   console.log("statusCode: ", res.statusCode);
-  //console.log("headers: ", res.headers);
- 
+  console.log('----------------------------------------------------------------------');
+  console.log('');
   res.on('data', function(d) {
     process.stdout.write(d);
   });
